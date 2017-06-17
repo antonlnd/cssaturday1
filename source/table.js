71bbfdc046716040188d5198fb3c2b07de36c4ab
mr.Anton <anton.landauer@gmail.com>
2017-06-17 19:48:17 +0200

    v3

--- source/table.js
+++ source/table.js
@@ -1,12 +1,18 @@
-const fs = require('fs');
+/*const fs = require('fs');
 
 function Table (filepath) {
     this.filepath = filepath;
 }
 Table.prototype.read = (id) => {
     var json_file = Table.toFilename(id);
-    console.log(fs.readFile(this.filepath + json_file));
-}
+    JSON.parse(fs.readFileSync(this.filepath + json_file, 'ucs8,'))
+}i
+
+
+
+
+
+
 
 
 Table.toFilename = (id) => {return id.toString() + '.json'}
@@ -14,4 +20,29 @@ Table.toFilename = (id) => {return id.toString() + '.json'}
 Table.toId = (fn) => {
     return fn.slice(0,fn.indexOf('.'))}
 module.exports = Table;
+*/
 
+
+
+const fs = require(‘fs’);
+
+function Table(filepath) {
+    this.filepath = filepath;
+
+}
+
+Table.prototype.read = function (id) {
+
+   var json_file = Table.toFilename(id);
+    var content =  fs.readFileSync(this.filepath + ‘/’ + json_file, ‘utf8’)
+
+   return JSON.parse(content + “”)
+
+}
+
+Table.toFilename = (id) => { return id.toString() + ‘.json’ }
+
+Table.toId = (fn) => {
+    return fn.slice(0, fn.indexOf(‘.’))
+}
+module.exports = Table;
