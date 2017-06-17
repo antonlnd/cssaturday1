f0ad81b862219e9162472434317042bca44c9f38
sid <sid@sids-Air.home>
2017-06-17 13:51:38 -0400

    finished part 1 spec 5

--- source/table.js
+++ source/table.js
@@ -1,30 +1,4 @@
-/*const fs = require('fs');
-
-function Table (filepath) {
-    this.filepath = filepath;
-}
-Table.prototype.read = (id) => {
-    var json_file = Table.toFilename(id);
-    JSON.parse(fs.readFileSync(this.filepath + json_file, 'ucs8,'))
-}i
-
-
-
-
-
-
-
-
-Table.toFilename = (id) => {return id.toString() + '.json'}
-
-Table.toId = (fn) => {
-    return fn.slice(0,fn.indexOf('.'))}
-module.exports = Table;
-*/
-
-
-
-const fs = require(‘fs’);
+const fs = require('fs');
 
 function Table(filepath) {
     this.filepath = filepath;
@@ -33,16 +7,25 @@ function Table(filepath) {
 
 Table.prototype.read = function (id) {
 
-   var json_file = Table.toFilename(id);
-    var content =  fs.readFileSync(this.filepath + ‘/’ + json_file, ‘utf8’)
+    var json_file = Table.toFilename(id);
+ 
+ try{var content =  fs.readFileSync(this.filepath + '/' + json_file, 'utf8')}
 
-   return JSON.parse(content + “”)
+ 
+ catch(err)
+{return;}
+     
+ 
+    
+    
+ 
+    return JSON.parse(content + "")  
 
 }
 
-Table.toFilename = (id) => { return id.toString() + ‘.json’ }
+Table.toFilename = (id) => { return id.toString() + '.json' }
 
 Table.toId = (fn) => {
-    return fn.slice(0, fn.indexOf(‘.’))
+    return fn.slice(0, fn.indexOf('.'))
 }
 module.exports = Table;
